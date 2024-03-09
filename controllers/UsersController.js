e56c70ba9e5da765b2b17e2795edefa2d6191af6
JustusOmbok <ombokjustus@gmail.com>
2024-03-09 00:51:02 -0800

    users retrieved

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,5 +1,5 @@
-import sha1 from 'sha1';
 import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
 
 const UsersController = {
   async postNew(req, res) {
@@ -37,6 +37,30 @@ const UsersController = {
       return res.status(500).json({ error: 'Internal server error' });
     }
   },
+
+  async getMe(req, res) {
+    const { 'x-token': token } = req.headers;
+
+    if (!token) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const userId = await redisClient.get(`auth_${token}`);
+    if (!userId) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const user = await dbClient.client
+      .db()
+      .collection('users')
+      .findOne({ _id: userId });
+
+    if (!user) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    return res.status(200).json({ id: user._id, email: user.email });
+  }
 };
 
 export default UsersController;
